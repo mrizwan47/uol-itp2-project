c1794d21bbcf11e00a22c27c6917214d0915209e
Muhammad Rizwan <m.rizwan_47@yahoo.com>
2020-09-16 20:29:10 +0500

    initial commit + stamp tool

---
+++ tools/stampTool.js
@@ -0,0 +1,110 @@
+function stampTool() {
+
+	this.name = "stampTool";
+	this.icon = "assets/stampTool.jpg";
+
+	// this changes in the javascript click handler. So storing it as
+	// a variable self now means we can still access it in the handler
+	var self = this;
+
+	// where was the mouse on the last time draw was called.
+	// set it to -1 to begin with
+	var previousMouseX = -1;
+	var previousMouseY = -1;
+
+	// Store uploaded stamp image and width/heigh settings for it
+	var stampImage = null;
+	var imageWidth = 30;
+	var imageHeight = 30;
+
+	this.draw = function() {
+
+		// display the last save state of pixels
+		updatePixels();
+
+		// do the drawing if the mouse is pressed 
+		// And image has been uploaded
+ 		if ( mouseIsPressed && stampImage !== null ) {
+			
+			// if the previous values are -1 set them to the current mouse location
+			if (previousMouseX == -1) {
+				previousMouseX = mouseX;
+				previousMouseY = mouseY;
+			}
+
+			// if there are values in the previous locations
+			// draw a line between them and the current positions
+			else {
+
+				image(stampImage, mouseX-(imageWidth/2), mouseY-(imageHeight/2), imageWidth, imageHeight);
+				previousMouseX = mouseX;
+				previousMouseY = mouseY;
+
+			}
+		}
+		// if the mouse isn't pressed reset the previous values to -1
+		else {
+			previousMouseX = -1;
+			previousMouseY = -1;
+		}
+
+		// after the drawing is done save the pixel state. We don't want the
+		// line of symmetry to be part of our drawing
+
+		loadPixels();
+
+		// push the drawing state so that we can set the stroke weight and colour
+		push();
+		strokeWeight(1);
+		stroke("black");
+
+		// show the selected/uploaded stamp image with cursor
+		if( stampImage !== null ){
+			image(stampImage, mouseX-(imageWidth/2), mouseY-(imageHeight/2), imageWidth, imageHeight);
+		}
+		
+		// return to the original stroke
+		pop();
+
+	};
+
+	
+	// when the tool is deselected update the pixels to just show the drawing and
+	// hide the line of symmetry. Also clear options
+	this.unselectTool = function() {
+
+		updatePixels();
+
+		// clear options
+		select(".options").html("");
+
+	};
+
+	// adds a button and click handler to the options area. When clicked
+	// toggle the line of symmetry between horizonatl to vertical
+	this.populateOptions = function() {
+
+		stampFileDiv = createDiv('Upload Image for Stamp: <br />');
+		input = createFileInput(function(file){
+			if (file.type === 'image') {
+				stampImage = createImg(file.data);
+			}
+		});
+		input.parent(stampFileDiv);
+		stampFileDiv.parent('toolsOptions');
+		
+
+		stampSizeDiv = createDiv('Size: <br />');
+		slider = createSlider(5, 150, 30);
+		slider.input(function(){
+			imageWidth = this.value();
+			imageHeight = this.value();
+		});
+		slider.parent(stampSizeDiv);
+		stampSizeDiv.parent('toolsOptions');
+
+
+
+	};
+
+}
